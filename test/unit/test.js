5a2430da20804971fb8680aebf6b0dfd994d446c
ruanyl <ruanyu1@gmail.com>
2014-04-17 02:12:43 +0300

    add test

--- test/unit/test.js
+++ test/unit/test.js
@@ -1,9 +1,17 @@
 var assert = require("assert");
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+var chai = require('chai');
+chai.should();
+
+var Matching = require("../../src/js/matching");
+
+describe('Matching', function() {
+  describe('#pointToSegCross', function() {
+    it('should return lat and lng of the cross', function() {
+      var cross = Matching.pointToSegCross(60.18845728866007, 24.834149479866028,
+                                           60.1886999769209, 24.834535717964172,
+                                           60.18804391441504, 24.83488440513611);
+      cross.should.have.property('lat').be.a('number');
+      cross.should.have.property('lng').be.a('number');
     });
   });
 });
