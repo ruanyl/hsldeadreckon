2755868451b5b83e3990a7782ec4696c0ff5d073
ruanyl <ruanyu1@gmail.com>
2014-04-16 23:54:33 +0300

    remove unused code

--- src/js/init.js
+++ src/js/init.js
@@ -3,6 +3,7 @@ var config = require('./config');
 function init() {
   map = L.map('map');
   map.on('load', setInitMap);
+  marker = L.marker([config.defaultConfig.latitude, config.defaultConfig.longitude]).addTo(map);
   map.setView([config.defaultConfig.latitude, config.defaultConfig.longitude], config.defaultConfig.zoom);
   L.tileLayer(config.cloudmade.url, {
     attribution: config.cloudmade.attribution
@@ -24,29 +25,11 @@ function setInitMap() {
     var longitude = config.defaultConfig.longitude;
     setPosition(latitude, longitude);
   }
-  //load geojson
-  $.ajax({
-    url: '/static/map2.json'
-  }).done(renderGeoData);
 }
 
 function setPosition(latitude, longitude) {
   map.setView([latitude, longitude], config.defaultConfig.zoom);
-  var marker = L.marker([latitude, longitude]).addTo(map);
+  marker.setLatLng([latitude, longitude]);
 }
 
-function renderGeoData(data) {
-  var latlngs = [];
-  var features = data.features[0].geometry.coordinates;
-  //var polyline = L.polyline(latlngs, {color: 'red', smoothFactor: 2}).addTo(map);
-  //for(var i=0; i < features.length; i++) {
-    //var latlng = L.latLng(features[i].geometry.coordinates[1], features[i].geometry.coordinates[0]);
-    //latlngs.push(latlng);
-    //polyline.addLatLng(latlng);
-    //map.fitBounds(polyline.getBounds());
-  //}
-  //console.log(latlngs);
-  var multiPolygon = L.multiPolygon(features, {color: 'red', smoothFactor: 2}).addTo(map);
-  map.fitBounds(multiPolygon.getBounds());
-}
 init();
